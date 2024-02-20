b2a0e458930d8f03cb64c572d4c55c5fc247bc16
estan <elise.stankus@gmail.com>
2024-02-19 18:14:19 -0800

    created instance of express and declared port

--- server.js
+++ server.js
@@ -2,3 +2,9 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
+
+//creating an instance of express
+const app = express();
+
+//declaring the PORT
+const PORT = process.env.PORT || 3001;
