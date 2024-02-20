6ecf8d44d15e60606d036b2326493c0b1231872d
estan <elise.stankus@gmail.com>
2024-02-19 20:59:52 -0800

    edited file path in get route

--- routes/api/notesRoutes.js
+++ routes/api/notesRoutes.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 // GET request for all notes
 router.get('/', (req, res) => {
-    fs.readFile('../../db/db.json', 'utf-8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
     if (err) {
         console.error(err);
       } else {
