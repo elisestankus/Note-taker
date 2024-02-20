b88f74cb04215b8b845a1ce19f5d5b58ed1000cd
estan <elise.stankus@gmail.com>
2024-02-19 20:57:21 -0800

    updated url for get route

--- routes/api/notesRoutes.js
+++ routes/api/notesRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const fs = require('fs');
 
 // GET request for all notes
-router.get('/notes', (req, res) => {
+router.get('/', (req, res) => {
     fs.readFile('../../db/db.json', 'utf-8', (err, data) => {
     if (err) {
         console.error(err);
