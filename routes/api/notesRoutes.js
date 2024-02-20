fe622fd8766b919efde0c86246915779ed1e1c13
estan <elise.stankus@gmail.com>
2024-02-19 20:26:22 -0800

    added get all notes route

---
+++ routes/api/notesRoutes.js
@@ -0,0 +1,16 @@
+//dependencies
+const router = require('express').Router();
+const fs = require('fs');
+
+// GET request for all notes
+router.get('/api/notes', (req, res) => {
+    fs.readFile('../../db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+      } else {
+        res.json(JSON.parse(data))
+      }
+    })
+});
+
+module.exports = router;
