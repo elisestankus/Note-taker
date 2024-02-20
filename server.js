56ecddaf452ae043b90572c574a2bed79a934a1d
estan <elise.stankus@gmail.com>
2024-02-19 18:16:27 -0800

    added middleware

--- server.js
+++ server.js
@@ -8,3 +8,13 @@ const app = express();
 
 //declaring the PORT
 const PORT = process.env.PORT || 3001;
+
+//middleware to parse incoming requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//middleware to use routes
+app.use(routes);
