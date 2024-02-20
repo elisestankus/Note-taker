482c4479e04839b686bee3ff07c03e1d9d9923ba
estan <elise.stankus@gmail.com>
2024-02-19 20:56:58 -0800

    fixed url

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@
 const router = require('express').Router();
 const notesRoutes = require('./notesRoutes');
 
-router.use('./notes', notesRoutes);
+router.use('/notes', notesRoutes);
 
 module.exports = router;
