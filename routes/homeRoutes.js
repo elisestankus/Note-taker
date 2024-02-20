f1a4aebdd306ec0ade2aafe9b604802d1ecba443
estan <elise.stankus@gmail.com>
2024-02-19 20:28:22 -0800

    fixed file path for index.html and notes.html

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,10 +2,11 @@
 const router = require('express').Router();
 const path = require('path');
 
+
 //get route for homepage
 router.get('/', async (req, res) => {
     try {
-        res.sendFile(path.join(__dirname, '/public/index.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     } catch (err) {
         res.status(500).json(err);
     }
@@ -14,7 +15,7 @@ router.get('/', async (req, res) => {
 //get route for notes page
 router.get('/notes', (req, res) => {
     try {
-        res.sendFile(path.join(__dirname, '/public/notes.html'));
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
     } catch (err) {
         res.status(500).json(err);
     }
