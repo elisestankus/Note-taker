8fdf2e23ccd17e648e39c52ba5192e9395bcd22b
estan <elise.stankus@gmail.com>
2024-02-19 19:24:31 -0800

    required path, added get route for notes page, and exported router

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,5 +1,6 @@
 //dependencies
 const router = require('express').Router();
+const path = require('path');
 
 //get route for homepage
 router.get('/', async (req, res) => {
@@ -9,3 +10,14 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+//get route for notes page
+router.get('/notes', (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, '/public/notes.html'));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
